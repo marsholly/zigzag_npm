079eef17cee7476d71f4d3b18f874857d2b6a18d
HONG ZHOU <holly_zhou@yahoo.com>
2016-10-30 22:55:27 -0700

    change let to var

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function zigzag(text, nRows) {
 	if (typeof text !== 'string') throw new Error('Input text must be string.');
 	if (typeof nRows !== 'number') throw new Error('Input nRows must be number.');
 
-	let arr = Array(nRows).fill('');
-	let loop = 2 * nRows - 2;
+	var arr = Array(nRows).fill('');
+	var loop = 2 * nRows - 2;
 	text.split('').forEach((letter, index) => {
 		x = index % loop;
 		row = x >= nRows ? (loop - x) : x;
